9fc756ace3d90651ca3a39e8bbb0728dd5b921e7
Manoj Eliger <eligermanoj@gmail.com>
2025-09-13 14:06:54 +0530

    todays 1st commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// let str="Aman" ;
+// if ((str[0]==='A' || str[0]==='a' ) && (str.length<=3)) {
+//     console.log("it is good string");
+// }else{
+//     console.log("it is not a good string ");
+// }
+
+let nam= prompt("Enter your Name");
+let age= prompt("Enter your age ");
+console.log(nam," is ",age," year old");
